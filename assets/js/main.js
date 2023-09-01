2c0470cd0174c72dec77c37c9dd9697eaa4be565
eligtj <eli_gtj@hotmail.com>
2023-09-01 14:59:16 -0300

    correção nos alerts

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,25 +6,25 @@ document.getElementById('btnSubscribe').addEventListener('click', () => {
 
     // Verifique se o campo 'nome' é alfanumérico
     if (!/^[A-Za-z0-9\s]+$/.test(nameField.value)) {
-        alert('O campo Nome deve conter apenas caracteres alfanuméricos.');
+        alert('O campo "Nome" deve conter apenas caracteres alfanuméricos.');
         return;
     }
 
     // Verifique se o campo 'email' contém um '@'
     if (emailField.value.indexOf('@') === -1) {
-        alert('O campo E-mail deve conter um "@".');
+        alert('O campo "E-mail" deve conter um "@".');
         return;
     }
 
     // Verifique se o campo 'level' contém apenas números
     if (!/^[0-9]+$/.test(levelField.value)) {
-        alert('O campo Level deve conter apenas números.');
+        alert('O campo "Level" deve conter apenas números.');
         return;
     }
 
     // Verifique se o campo 'personagem' é alfanumérico
     if (!/^[A-Za-z0-9\s]+$/.test(characterField.value)) {
-        alert('O campo Personagem deve conter apenas caracteres alfanuméricos.');
+        alert('O campo "Personagem" deve conter apenas caracteres alfanuméricos.');
         return;
     }
 
